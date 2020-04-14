57b83403b6ee6bbf8d7a9af20f54894785a6bcc6
Ram Murali <mailmram@gmail.com>
2020-04-14 07:55:38 -0700

    Update

---
+++ CTCI/Stack and Queues/QueueStack.py
@@ -0,0 +1,51 @@
+
+class stack:
+    class node:
+        def __init__(self,data):
+            self.next = None
+            self.data = data
+    
+    def __init__(self):
+        self.top = None
+
+    def push(self,node: node):
+        if self.top == None:
+            self.top = node
+        else:
+            node.next = self.top
+            self.top = node
+
+    def pop(self):
+        if self.top == None:
+            print("Stack Empty")
+        else:
+            lastElm = self.top.data
+            self.top = self.top.next
+            return lastElm
+            
+
+    def peek(self):
+        if self.top == None:
+            print("Stack Empty")
+        else:
+            print("Peek Item", self.top.data)
+    
+    def itreateStack(self):
+        res = []
+        temp = self.top
+        while temp:
+            res.append(str(temp.data))
+            temp = temp.next
+        print('<-'.join(res))
+
+myStack = stack()
+myStack.push(myStack.node(5))
+myStack.push(myStack.node(6))
+myStack.push(myStack.node(7))
+myStack.itreateStack()
+mynewStack = stack()
+mynewStack.push(mynewStack.node(myStack.pop()))
+mynewStack.push(mynewStack.node(myStack.pop()))
+mynewStack.push(mynewStack.node(myStack.pop()))
+mynewStack.itreateStack()
+    
